49360dade0b2f696f4850258b74da5fb2a2cfa82
Sudip Mandal <mailme.sudip.mandal@gmail.com>
2021-06-26 15:10:35 -0700

    Update startupscript.sh

--- startupscript.sh
+++ startupscript.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-blobfuse /backup --container-name=$AZURE_CONTAINER_NAME --tmp-path=/mnt/blobfusetmp
+sudo -u www-data blobfuse /backup --container-name=$AZURE_CONTAINER_NAME --tmp-path=/mnt/blobfusetmp -o uid=33 -o gid=33
+
 set -eu
 # version_greater A B returns whether A > B
 version_greater() {
